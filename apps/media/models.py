d49bff0381ad2f6d518cb3e4f3dd76e79f6a6b24
codeercpp-byte <codeercpp@gmail.com>
2026-02-14 13:51:46 +0500

    edit2

--- apps/media/models.py
+++ apps/media/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class MediaVideo(models.Model):
+    title = models.CharField(max_length=255)
+    link = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
