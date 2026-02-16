86c8e573e60ef7545865fc7b48c6d7f001788055
codeercpp-byte <codeercpp@gmail.com>
2026-02-16 12:39:37 +0500

    edit2

--- config/settings.py
+++ config/settings.py
@@ -23,8 +23,8 @@ DEBUG = False
 ALLOWED_HOSTS = [
     '127.0.0.1', 
     'localhost', 
-    'u-ff.uz',
-    'www.u-ff.uz',
+    'oqdaryommtb.uz',
+    'www.oqdaryommtb.uz',
     '85.239.58.108',
 ]
 
@@ -34,8 +34,8 @@ CSRF_TRUSTED_ORIGINS = [
     
     'https://127.0.0.1',
     'https://localhost',
-    'https://u-ff.uz',
-    'https://www.u-ff.uz',
+    'https://oqdaryommtb.uz',
+    'https://www.oqdaryommtb.uz',
 ]
 
 LOGIN_URL = 'sign-in'
